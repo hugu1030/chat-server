265ab7f90d38c1a86718b360fca1b6340b472a2f
hugu1030 <kelodon1030@ymobile.ne.jp>
2020-03-09 19:00:17 +0900

    main.goにCORS機能を追加

--- Login/main.go
+++ Login/main.go
@@ -1,38 +1,38 @@
 package main
 
-import ( 
+import (
+	"./login"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
-	"./login"
 )
 
 func forCORS(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Credentials","true")
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-        // プリフライトリクエストの対応
-        if r.Method == "OPTIONS" {
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		// プリフライトリクエストの対応
+		if r.Method == "OPTIONS" {
 			log.Println("OPTIONS")
-            w.WriteHeader(http.StatusOK)
-            return
-        }
-        next.ServeHTTP(w, r)
-        return
-    })
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+		return
+	})
 }
 
 func main() {
 	router := mux.NewRouter()
 	router.Use(forCORS)
 
-	router.HandleFunc("/login",login.ReturnCanLogin)
-	router.HandleFunc("/newLogin",login.ReturnMadeAccount)
-	router.HandleFunc("/resetLogin",login.ReturnResetAccount)
-	log.Fatal(http.ListenAndServe(":8081",router))
+	router.HandleFunc("/login", login.ReturnCanLogin)
+	router.HandleFunc("/newLogin", login.ReturnMadeAccount)
+	router.HandleFunc("/resetLogin", login.ReturnResetAccount)
+	log.Fatal(http.ListenAndServe(":8081", router))
 
 	println("start server.")
 }
