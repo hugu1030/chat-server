4d777a5a8271bf7c19a5e43830b4666359f41953
hugu1030 <kelodon1030@ymobile.ne.jp>
2020-03-09 18:04:05 +0900

    loginを追加

--- Login/login/login.go
+++ Login/login/login.go
@@ -6,11 +6,23 @@ import (
 	_"github.com/lib/pq"
 	"encoding/json"
 	"log"
+	"io/ioutil"
 )
 
 type User struct {
-	Id   int
-	Name string
+	Id   int      `json:"id"`
+	Name string   `json:"name"`
+}
+
+type Pass struct {
+	Mail string
+	Password string    
+}
+
+type AllInfo struct {
+	Name   string   
+	Mail   string
+	Password  string
 }
 
 var Db *sql.DB
@@ -21,48 +33,75 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+	err = Db.Ping()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func ReturnCanLogin(w http.ResponseWriter,r *http.Request){
-	  e := r.ParseForm()
-	  log.Println(e)
-	  
+
+	  body,_ := ioutil.ReadAll(r.Body)
 	  u := User{}
-	  err := Db.QueryRow("SELECT id,name FROM chatlogin WHERE mail = ? && password = ? ",r.FormValue("mail"),r.FormValue("password")).Scan(&u.Id,&u.Name)
+	  p := Pass{}
+	
+	  json.Unmarshal(body,&p)     
+	  
+	  err := Db.QueryRow("SELECT id,name FROM chatlogin WHERE mail = $1 AND password = $2 ", p.Mail, p.Password).Scan(&u.Id,&u.Name)
 
       if err != nil{
-		  panic(err)
-	  }else{
-		 log.Println(u)
-		 log.Println(json.NewEncoder(w).Encode(u))
-		 json.NewEncoder(w).Encode(u)
+		  w.WriteHeader(http.StatusInternalServerError)
+		//   w.Write([]byte("No rows in result"))
+  		 return
 	  }
+		w.Header().Set("Content-Type","application/json")
+		log.Println("u:",u)
+		json.NewEncoder(w).Encode(u)
 }
 
-func ReturnMadeAccount(w http.ResponseWriter, r *http.Request){    
-	e := r.ParseForm()
-	log.Println(e)
-	// category := r.Form.Get("category")
 
-	stmt,err := Db.Prepare("INSERT INTO postgres (name,mail,password) VALUES(? ,? ,?)")
-	if err != nil {
-		return
-	}
-	defer stmt.Close()
+func ReturnMadeAccount(w http.ResponseWriter, r *http.Request){
+	
+	body,_ := ioutil.ReadAll(r.Body)
+    allinfo := AllInfo{}
+	json.Unmarshal(body,&allinfo)
     u := User{}
-	ret,err1 := stmt.Exec(r.FormValue("name"),r.FormValue("mail"),r.FormValue("password"))
+	
+	isPassword := Db.QueryRow("SELECT id FROM chatlogin WHERE mail = $1", allinfo.Mail).Scan(&u.Id)
+	log.Println(u.Id)
+	log.Println(allinfo.Mail)
+	log.Println(isPassword)
+	if isPassword == nil {
+		log.Println("このメールアドレスは既に登録されています")
+		w.WriteHeader(501)
+       return 
+	}else{
+	stmt,err := Db.Prepare("INSERT INTO chatlogin (name,mail,password) VALUES($1,$2,$3)")
+	if err != nil {
+		log.Println("データベース作成で失敗しました")
+		w.WriteHeader(http.StatusInternalServerError)
+		return 
+	}
+
+	// defer stmt.Close()
+
+	_,err1 := stmt.Exec(allinfo.Name,allinfo.Mail,allinfo.Password) 
 	if(err1 != nil){
+	   log.Println("データベース挿入に失敗しました")
+	   w.WriteHeader(http.StatusInternalServerError)
+	   return
+	}
+
+	err2 := Db.QueryRow("SELECT id,name FROM chatlogin WHERE mail = $1 AND name = $2 AND password = $3 ", allinfo.Mail,allinfo.Name,allinfo.Password).Scan(&u.Id,&u.Name)
+	if err2 != nil {
+		log.Println("データベース取り出しに失敗しました")
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	log.Println(ret)
-
-	err2 := Db.QueryRow("SELECT id,name FROM postgres WHERE mail = ? && name = ? && password = ? ", r.FormValue("mail"),r.FormValue("name"),r.FormValue("password")).Scan(&u.Id,&u.Name)
-	if err2 != nil{
-		return 
-	}else{
 		json.NewEncoder(w).Encode(u)
 	}
 }
+
 func ReturnResetAccount(w http.ResponseWriter, r *http.Request){
 	e := r.ParseForm()
 	log.Println(e)
